5222afeb977a1aa8649bc4d5360baaad4b3e5953
peco2282 <87461070+peco2282@users.noreply.github.com>
2023-03-30 09:46:14 +0900

    [add] Add license to header.

--- src/main/java/com/github/peco2282/priceencyclopedia/KeyHandler.java
+++ src/main/java/com/github/peco2282/priceencyclopedia/KeyHandler.java
@@ -1,16 +1,35 @@
+/*
+ * PriceEncyclopedia
+ * Copyright (c) 2023 peco2282
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package com.github.peco2282.priceencyclopedia;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.resources.language.I18n;
 
 public class KeyHandler {
-  public static KeyMapping hudKey;
+	public static KeyMapping hudKey;
 
-  public static void init() {
-    hudKey = new KeyMapping(
-        I18n.get("priceencyclopedia.key.keyhandle"),
-        'K',
-        I18n.get("priceencyclopedia.key.keyhandle.description")
-    );
-  }
+	public static void init() {
+		hudKey = new KeyMapping(
+			I18n.get(PriceEncyclopedia.MODID + ".key.keyhandle"),
+			'K',
+			I18n.get(PriceEncyclopedia.MODID + ".key.keyhandle.description")
+		);
+	}
 }
--- src/main/java/com/github/peco2282/priceencyclopedia/config/ConfigFile.java
+++ src/main/java/com/github/peco2282/priceencyclopedia/config/ConfigFile.java
@@ -1,3 +1,22 @@
+/*
+ * PriceEncyclopedia
+ * Copyright (c) 2023 peco2282
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package com.github.peco2282.priceencyclopedia.config;
 
 import com.github.peco2282.priceencyclopedia.PriceEncyclopedia;
@@ -60,14 +79,6 @@ public class ConfigFile {
     }
   }
 
-  public boolean isLoaded() {
-    return loaded;
-  }
-
-  public String getReason() {
-    return reason;
-  }
-
   public static File getFile() {
     PriceEncyclopedia.getLOGGER().warn("getFile() start.");
     File dir = new File("config");
@@ -104,6 +115,14 @@ public class ConfigFile {
       exception.printStackTrace(System.err);
     }
     return map;
+  }
+
+  public boolean isLoaded() {
+    return loaded;
+  }
+
+  public String getReason() {
+    return reason;
   }
 
   public ArrayList<Map<String, String>> save() {
