90183c020e4a5130bef0c0e308e8fad6c7821891
peco2282 <87461070+peco2282@users.noreply.github.com>
2023-04-11 19:56:28 +0900

    [remove] remove unused event.

--- src/main/java/com/github/peco2282/priceencyclopedia/key/KeyInputEvent.java
+++ src/main/java/com/github/peco2282/priceencyclopedia/key/KeyInputEvent.java
@@ -20,19 +20,10 @@
 package com.github.peco2282.priceencyclopedia.key;
 
 import com.github.peco2282.priceencyclopedia.PriceEncyclopedia;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class KeyInputEvent {
-	PoseStack stack;
-
-	@SubscribeEvent
-	public void o(RenderGuiOverlayEvent.Post event) {
-		stack = event.getPoseStack();
-	}
-
 	@SubscribeEvent
 	public void onKeyPressed(final InputEvent.Key key) {
 		if (key.getAction() == 1 && KeyHandler.getKeyMapping() != null && key.getKey() == KeyHandler.getKeyMapping().getKey().getValue()) {
